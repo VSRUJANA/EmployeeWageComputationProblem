169f91c4b51d466c711427bf5ae99e2db6b44048
VSRUJANA <srujana.valavala@gmail.com>
2020-09-27 18:05:18 +0530

    Add project files.

---
+++ Employee_Wage_Computation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Computation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+        }
+    }
+}
