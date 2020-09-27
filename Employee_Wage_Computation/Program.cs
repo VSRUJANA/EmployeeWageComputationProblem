7b4e8c2362aae1e6776438b5d4a08ec387024ef7
VSRUJANA <srujana.valavala@gmail.com>
2020-09-27 18:15:45 +0530

    [srujana] Refactor. Employee Attendance Program added (UC-1)

--- Employee_Wage_Computation/Program.cs
+++ Employee_Wage_Computation/Program.cs
@@ -4,9 +4,24 @@ namespace Employee_Wage_Computation
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Employee Wage Computation Program!");
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Employee Wage Computation Program");
+                Console.WriteLine(Attendance());
+            }
+
+
+            static string Attendance()
+            {
+                int isPresent = 1;
+                Random rand = new Random();
+                int empCheck = rand.Next(0, 2);
+                //1 => present and 0 => absent
+                if (empCheck == isPresent)
+                    return "Employee is present";
+                else
+                    return "Employee is absent";
+            }
         }
     }
-}
+    
