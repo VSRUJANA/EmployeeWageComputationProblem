56ad56d887e2800e2243a174a65201360a0bb4e0
VSRUJANA <srujana.valavala@gmail.com>
2020-09-27 18:48:06 +0530

    [srujana] Refactor. Employee Monthly Wage Calculation program added (UC-5)

--- Employee_Wage_Computation/Program.cs
+++ Employee_Wage_Computation/Program.cs
@@ -7,29 +7,34 @@ namespace Employee_Wage_Computation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            Console.WriteLine("Employee wage: " + EmpDailyWageUsingSwitch());
+            Console.WriteLine("Employee monthly wage: " + EmployeeMonthlyWageComputation());
         }
-        static int EmpDailyWageUsingSwitch()
+        static int EmployeeMonthlyWageComputation()
         {
+            int isFullTime = 1;
             int wagePerHour = 20;
             int workHrs = 0;
+            int WorkingDays = 0;
+            int empCheck = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            //switch case to calculate wages
-            switch (empCheck)
+            for (int day = 0; day < 20; day++)
             {
-                case 0:         //0. Full time present.
-                    workHrs = 8;
-                    break;
-                case 1:         //1. Part time present.
-                    workHrs = 4;
-                    break;
-                default:        // Absent
-                    workHrs = 0;
-                    break;
+                empCheck = random.Next(0, 2);
+                WorkingDays = WorkingDays + empCheck;
             }
-            int empWage = workHrs * wagePerHour;
-            return empWage;
+            Console.WriteLine("No. of days worked in the month :" + WorkingDays);
+            int empType = random.Next(0, 2);
+            if (empType == isFullTime)
+            {
+                workHrs = 8;
+                Console.WriteLine("Emp Type : Full time Employee");
+            }
+            else
+            {
+                workHrs = 4;
+                Console.WriteLine("Emp Type : Part time Employee");
+            }
+            return workHrs * WorkingDays * wagePerHour;
         }
     }
 }
